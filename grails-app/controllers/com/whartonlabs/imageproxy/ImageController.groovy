4c06248996abbbdf9e7c360498184342ed116fba
andrewwharton <andrew@whartonlabs.com>
2013-06-12 22:29:08 +0300

    added new trim and crop functionality

--- grails-app/controllers/com/whartonlabs/imageproxy/ImageController.groovy
+++ grails-app/controllers/com/whartonlabs/imageproxy/ImageController.groovy
@@ -1,37 +1,48 @@
 package com.whartonlabs.imageproxy
 
+import javax.imageio.ImageIO
+import java.awt.image.BufferedImage
+
 class ImageController {
 
     ImageService imageService
 
+    static final List TRIM_PARAMS = [
+        "trim_top", "trim_right", "trim_bottom", "trim_left"
+    ]
+
     static final List CROP_PARAMS = [
-        "top", "right", "bottom", "left"
+        "crop_ratio", "crop_x_offset", "crop_y_offset", "crop_shrink"
     ]
 
     static final List SCALE_PARAMS = [
         "width", "height"
     ]
 
-    /**
-     *
-     *
-     * Example URI: /image/suite/letterpress-invitation.jpg?top=10&bottom=20&left=30&right=50&width=1200
-     */
     def index() {
 
-        byte[] image = imageService.getOriginalImageData(params.pathToFile)
+        byte[] imageBytes = imageService.getOriginalImageData(params.pathToFile)
+
+        InputStream stream = new ByteArrayInputStream(imageBytes);
+        BufferedImage originalImage = ImageIO.read(stream);
+        BufferedImage processedImage
+
+        if(params.any{ it.key in TRIM_PARAMS }) {
+            processedImage = imageService.trim(originalImage, params)
+        }
 
         if(params.any{ it.key in CROP_PARAMS }) {
-            image = imageService.crop(image, params)
+            processedImage = imageService.crop(originalImage, params)
         }
 
         if(params.any{ it.key in SCALE_PARAMS }) {
-            image = imageService.scale(image, params)
+            processedImage = imageService.scale(processedImage, params)
         }
 
+        byte[] outputBytes = processedImage ? imageService.writeToByteArray(processedImage, 0.95f) : imageBytes
         response.contentType = "image/jpeg"
-        response.contentLength = image.length
-        response.outputStream << image
+        response.contentLength = outputBytes.length
+        response.outputStream << outputBytes
         response.outputStream.flush()
     }
 }
--- grails-app/services/com/whartonlabs/imageproxy/ImageService.groovy
+++ grails-app/services/com/whartonlabs/imageproxy/ImageService.groovy
@@ -19,31 +19,6 @@ class ImageService {
     GrailsApplication grailsApplication
 
     /**
-     * Scales the image using the given width and height, scales proportionally if only one is given
-     *
-     * @param imageBytes
-     * @param params
-     * @return
-     */
-    byte[] scale(byte[] imageBytes, Map params) {
-
-        InputStream stream = new ByteArrayInputStream(imageBytes);
-        BufferedImage originalImage = ImageIO.read(stream);
-        BufferedImage processedImage
-
-        if (params.width && params.height){
-            processedImage = Scalr.resize(originalImage, Scalr.Method.ULTRA_QUALITY, Scalr.Mode.FIT_EXACT, params.width as int, params.height as int)
-        } else if(params.width || params.height) {
-            processedImage = Scalr.resize(originalImage, Scalr.Method.ULTRA_QUALITY, params.width ? Scalr.Mode.FIT_TO_WIDTH : Scalr.Mode.FIT_TO_HEIGHT, params.width ? params.width as int: params.height as int)
-        } else {
-            return imageBytes
-        }
-
-        return writeToByteArray(processedImage, 0.95f)
-
-    }
-
-    /**
      * Trims the edges from the corresponding edge by the given number of pixels
      *
      * @param params.top Number of pixels to trim from the top edge
@@ -52,21 +27,89 @@ class ImageService {
      * @param params.right Number of pixels to trim from the right edge
      * @return the cropped JPEG image bytes
      */
-    byte[] crop(byte[] imageBytes, Map params) {
-
-        InputStream stream = new ByteArrayInputStream(imageBytes);
-        BufferedImage originalImage = ImageIO.read(stream);
-
+    BufferedImage trim(BufferedImage image, Map params) {
         Map cropWindow = [
-            x: params.left ? Integer.parseInt(params.left) : 0,
-            y: params.top ? Integer.parseInt(params.top) : 0,
+            x: params.trim_left ? Integer.parseInt(params.trim_left) : 0,
+            y: params.trim_top ? Integer.parseInt(params.trim_top) : 0,
         ]
-        cropWindow.width = (originalImage.width - cropWindow.x - (params.right ? Integer.parseInt(params.right) : 0))
-        cropWindow.height = (originalImage.height - cropWindow.y - (params.bottom ? Integer.parseInt(params.bottom) : 0))
+        cropWindow.width = (originalImage.width - cropWindow.x - (params.trim_right ? Integer.parseInt(params.trim_right) : 0))
+        cropWindow.height = (originalImage.height - cropWindow.y - (params.trim_bottom ? Integer.parseInt(params.trim_bottom) : 0))
 
-        BufferedImage processedImage = Scalr.crop(originalImage, cropWindow.x, cropWindow.y, cropWindow.width, cropWindow.height)
+        return Scalr.crop(originalImage, cropWindow.x, cropWindow.y, cropWindow.width, cropWindow.height)
+    }
 
-        return  writeToByteArray(processedImage, 0.95f)
+    /**
+     * Crops the images based on the desired aspect ratio, x and y offset and the scaling down of the crop window
+     *
+     * By default, the crop window will be centered vertically and horizontally,
+     * scaled to the maximum size that the specified aspect ration allows, given the aspect ratio
+     * of the input image.
+     *
+     * @param params.crop_ratio The desired aspect ratio of the output image eg. 4:3 -> 1.33, 16:9 -> 1.78, square -> 1
+     * @param params.crop_x_offset the
+     * @param params.crop_y_offset Number of pixels to trim from the left edge
+     * @param params.crop_shrink Number of pixels to trim from the right edge
+     * @return the cropped BufferedImage
+     */
+    BufferedImage crop(BufferedImage originalImage, Map params) {
+
+        if(!params.crop_ratio) {
+            return originalImage
+        } else {
+
+            // parse input values and add defaults where not specified
+            double desiredRatio = params.crop_ratio as double
+            int xOffset = params.crop_x_offset ? params.crop_x_offset as int : 0
+            int yOffset = params.crop_y_offset ? params.crop_y_offset as int : 0
+            double shrink = params.crop_shrink ? params.crop_shrink as double : 1.0
+
+            // calculate the crop window
+            Map cropWindow = [
+                x: 0,
+                y: 0,
+                width: originalImage.width,
+                height: originalImage.height
+            ]
+
+            double originalImageRatio = (originalImage.width / originalImage.height) as double
+
+            if(originalImageRatio > desiredRatio) {
+                cropWindow.width = (desiredRatio / originalImageRatio) * originalImage.width
+                int numberOfXPixelsToRemove = originalImage.width - cropWindow.width
+                cropWindow.x = numberOfXPixelsToRemove / 2 // horizontally center the crop
+            } else {
+                cropWindow.height = (originalImageRatio / desiredRatio) * originalImage.height
+                int numberOfYPixelsToRemove = originalImage.height - cropWindow.height
+                cropWindow.y = numberOfPixelsYToRemove / 2 // horizontally center the crop
+            }
+
+            if(xOffset) {
+                cropWindow.x = cropWindow.x + xOffset
+            }
+
+            return Scalr.crop(originalImage, cropWindow.x as int, cropWindow.y as int, cropWindow.width as int, cropWindow.height as int)
+
+        }
+
+    }
+
+    /**
+     * Scales the image using the given width and height, scales proportionally if only one is given
+     *
+     * @param image
+     * @param params
+     * @return
+     */
+    BufferedImage scale(BufferedImage image, Map params) {
+
+        if (params.width && params.height){
+            return Scalr.resize(image, Scalr.Method.ULTRA_QUALITY, Scalr.Mode.FIT_EXACT, params.width as int, params.height as int)
+        } else if(params.width || params.height) {
+            return Scalr.resize(image, Scalr.Method.ULTRA_QUALITY, params.width ? Scalr.Mode.FIT_TO_WIDTH : Scalr.Mode.FIT_TO_HEIGHT, params.width ? params.width as int: params.height as int)
+        } else {
+            return image
+        }
+
     }
 
     /**
